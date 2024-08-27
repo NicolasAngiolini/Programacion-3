44e9e33f9acb50f6bdfc261f3a2b6dec00f7eb04
Nicolas <nicolas.angiolini2@gmail.com>
2024-08-27 19:51:20 -0300

    Punto5 MergeSort

--- src/main/java/tp2/tp2/Main.java
+++ src/main/java/tp2/tp2/Main.java
@@ -7,11 +7,12 @@ public class Main {
         int[] arreglo = {1, 2, 3, 4, 5};
 
 
-
         System.out.println(estaOrdenado(arreglo));
         System.out.println(buscarElemento(arreglo, 2));
         System.out.println(convertirBinario(26));
         imprimirFibonacci(7);
+        System.out.println(estaEnPosicion(arreglo));
+
     }
 
     public static boolean estaOrdenado(int[] arr) {
@@ -28,6 +29,11 @@ public class Main {
         }
     }
 
+    public static boolean estaEnPosicion(int[] arr) {
+        return estaEnPosicionRecursivo(arr, 0);
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
     //O(n)donde n es el arr.length- Es mas costoso en recursos(memoria)
 
     private static boolean estaOrdenadoRecursivo(int[] arr, int i) {
@@ -37,7 +43,8 @@ public class Main {
             return false;
         return estaOrdenadoRecursivo(arr, i + 1);
     }
-//O(n)
+
+    //O(n)
     private static int buscarElementoRecursivo(int[] arr, int n, int i) {
         if (i >= arr.length - 1)
             return -1;
@@ -46,7 +53,8 @@ public class Main {
         return buscarElementoRecursivo(arr, n, i + 1);
 
     }
-//
+
+    //
     public static String convertirBinario(int n) {
         if (n == 0)
             return "0";
@@ -62,4 +70,24 @@ public class Main {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
+
+    private static boolean estaEnPosicionRecursivo(int[] arr, int i) {
+        if (i == arr[i])
+            return true;
+        else if (i >= arr.length - 1)
+            return false;
+        return estaEnPosicionRecursivo(arr, i + 1);
+    }
+
+    public void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+            // merge(array, left, mid, right);
+        }
+    }
+
 }
+
